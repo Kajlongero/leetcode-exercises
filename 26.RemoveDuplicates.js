a4f816053e1b494d7904e3259c31c33a4abdae88
Kajlongero <143892901+Kajlongero@users.noreply.github.com>
2024-09-03 14:24:34 -0400

    Update 26.RemoveDuplicates.js

--- 26.RemoveDuplicates.js
+++ 26.RemoveDuplicates.js
@@ -16,20 +16,3 @@ const rDup = (nums) => {
 
   return set.size;
 };
-
-console.log(rDup([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
-
-let k = 1;
-
-for (let i = 1; i < nums.length; i++) {
-  if (nums[i] !== nums[i - 1]) {
-    nums[k] = nums[i];
-    k++;
-  }
-}
-
-return k;
-
-[0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
-[0, 1, 2, 3, 4];
-[0, 1, 2, 3, 4];
