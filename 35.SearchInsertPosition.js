af047fafc17b3e8b5986b5331d2b54172038707d
Kajlongero <dev.kajlongero@gmail.com>
2024-09-16 14:39:21 -0400

    35.SearchInsertPosition - Same as a binary search but returning the left pointer

---
+++ 35.SearchInsertPosition.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+const searchInsert = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((right + left) / 2);
+
+    if (nums[middle] < target) {
+      left = middle + 1;
+    } else if (nums[middle] > target) {
+      right = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+
+  return left;
+};
